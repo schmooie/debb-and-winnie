0816cc4eebcf68fa32531d62221d173db0a85af4
Sam Lau <samuel.kwun.lau@gmail.com>
2024-03-31 16:09:55 -0400

    feat: build out basic audio player

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import nycExesSong from '/audio/nyc_exes.m4a'
 
 document.querySelector('#profile_lastloggedin').innerHTML = `
   Last Login: ${(new Date()).toLocaleDateString()
 }
 `
+
+Amplitude.init({
+  songs: [
+    {
+      name: 'NYC Exes',
+      artist: 'Debb and Winnie',
+      album: 'Ancram EP',
+      url: './audio/nyc_exes.m4a',
+      cover_art_url: './img/debbAndWinnie.jpg'
+    }
+  ]
+})
