9a9f2e063a00669e3054ab5e357e1311a7b458e2
Sam Lau <samuel.kwun.lau@gmail.com>
2024-03-31 14:24:06 -0400

    feat: create basic MySpace layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myspace: {
+          'blue-100': '#d5e8fb', // lightest blue
+          'blue-200': '#b1d0f0', // left col blue
+          'blue-300': '#6699cc', // right col blue + navbar
+          'blue-500': '#003399', // darkest blue
+          'blue-600': '#04369a', // dark link blue
+          'blue-700': '#336699', // blue text inside col
+          'blue-800': '#1548ae',
+
+          'orange-100': '#ffcc99',
+          'orange-200': '#ff6600',
+          'orange-300': '#ff9933',
+
+          'red-600': '#cc0000',
+
+          'music': '#990033'
+        },
+        dark: '#151719',
+        'dark-ish': '#111213',
+        'myspace-light': '#e5e5e5',
+      },
+      width: {
+        800: '800px',
+      },
+    },
   },
   plugins: [],
 }
